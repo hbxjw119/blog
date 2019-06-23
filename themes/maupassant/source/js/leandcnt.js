d695aa696f800d256f5091dc31e230abad9f98dc
xujiwei <xujiwei@didichuxing.com>
2019-06-23 21:44:56 +0800

    update

--- themes/maupassant/source/js/leandcnt.js
+++ themes/maupassant/source/js/leandcnt.js
@@ -1,5 +1,10 @@
 var visitorCount = function(app_id, app_key, obj_id) {
-	AV.init({appId:app_id, appKey: app_key});
+	AV.init({appId:app_id, appKey: app_key, serverURLs: {
+		push:'https://avoscloud.com',
+		stats: 'https://avoscloud.com',
+	    engine: 'https://avoscloud.com',
+	    api: 'https://avoscloud.com',},
+	});
 	var cnt=AV.Object.createWithoutData('total_pv',obj_id);
 	cnt.increment('pv',1);
 	cnt.save();
